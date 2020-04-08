80ecc1b725c1b101e466cbfd98e00361a1e3030d
user2 <user2@DESKTOP-BQDDAC1>
2020-04-08 20:07:59 +0900

    project start

---
+++ src/com/kh/project/model/vo/Player.java
@@ -0,0 +1,5 @@
+package com.kh.project.model.vo;
+
+public class Player {
+
+}
---
+++ src/com/kh/project/run/Run.java
@@ -0,0 +1,10 @@
+package com.kh.project.run;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
