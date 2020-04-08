945f3f6328f69305456df03506c5ab93c9f6a810
user2 <user2@DESKTOP-G2Q4DME>
2020-04-08 21:33:59 +0900

    test

--- src/com/kh/project/view/MakeIdView.java
+++ src/com/kh/project/view/MakeIdView.java
@@ -12,7 +12,7 @@ public class MakeIdView extends JPanel{
 		JLabel label = new JLabel(new ImageIcon(new ImageIcon("C:\\Users\\user2\\git\\OZproject\\src\\image\\start\\로그인간판.png").getImage().getScaledInstance(400, 400, 0)));
 		label.setBounds(-30, 50, 400, 400);
 		this.add(label);
-		
+		System.out.println("");
 		
 	}
 }
