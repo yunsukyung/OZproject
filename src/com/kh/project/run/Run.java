fbcebb5065f534477f31568a49a797889f451c64
jino0 <jino0@LAPTOP-ECH7OLH4>
2020-04-08 21:09:34 +0900

    4월8일 9:09분  강제로 보내라함

--- src/com/kh/project/run/Run.java
+++ src/com/kh/project/run/Run.java
@@ -1,10 +1,14 @@
 package com.kh.project.run;
 
+import com.kh.project.view.LoginView;
+
 public class Run {
 
 	public static void main(String[] args) {
 		
-
+		new LoginView(null);
+		
+		
 	}
 
 }
---
+++ src/com/kh/project/view/LoginView.java
@@ -0,0 +1,19 @@
+package com.kh.project.view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LoginView extends JPanel{
+
+	public LoginView(JFrame mf) {
+		
+		this.setLayout(null);
+		JLabel label = new JLabel(new ImageIcon(new ImageIcon("C:\\Users\\jino0\\git\\OZproject\\src\\image\\start\\로그인간판.png").getImage().getScaledInstance(360, 640, 0)));
+		label.setBounds(0, 0, 360, 640);
+		this.add(label);
+		
+	}
+	
+}
