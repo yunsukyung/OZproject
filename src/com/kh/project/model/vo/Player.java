2b9b22bc0da6a22e297899c2ec987d65b0d987ec
hailey <hailey@192.168.100.12>
2020-04-09 13:26:52 +0900

    Player
    field 전부 private으로 바꿈

--- src/com/kh/project/model/vo/Player.java
+++ src/com/kh/project/model/vo/Player.java
@@ -1,17 +1,18 @@
 package com.kh.project.model.vo;
 
-
 public class Player {
-	private String userId;		//유저아이디
-	private String userPwd;		//유저비밀번호
-	private String userName;	//유저 닉네임
-	private String phoneNumber;	//핸드폰번호
-	private String email;		//이메일
-	public static int pearl;	//진주
-	public static int garbage;	//쓰레기
-	public static int sp;		//공격력
-	public static int satiety;	//포만감
-
+	
+	private String userId;		//아이디
+	private String userPwd;		//비밀번호
+	private String userName;	//이름
+	private String phoneNumber;	//휴대폰 번호
+	private String email;
+	//인벤
+	private int pearl;	//진주 갯수
+	private int garbage;	//쓰레기 갯수
+	private int sp;		//공격력
+	private int satiety;	//포만감
+	
 	public Player() {}
 
 	public Player(String userId, String userPwd, String userName, String phoneNumber, String email, int pearl,
@@ -22,66 +23,84 @@ public class Player {
 		this.userName = userName;
 		this.phoneNumber = phoneNumber;
 		this.email = email;
-		Player.pearl = pearl;
-		Player.garbage = garbage;
-		Player.sp = sp;
-		Player.satiety = satiety;
+		this.pearl = pearl;
+		this.garbage = garbage;
+		this.sp = sp;
+		this.satiety = satiety;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+
 	public String getUserPwd() {
 		return userPwd;
 	}
-	public void setUserPwd(String userPwd) {
-		this.userPwd = userPwd;
-	}
+
 	public String getUserName() {
 		return userName;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+
 	public String getEmail() {
 		return email;
 	}
+
+	public int getPearl() {
+		return pearl;
+	}
+
+	public int getGarbage() {
+		return garbage;
+	}
+
+	public int getSp() {
+		return sp;
+	}
+
+	public int getSatiety() {
+		return satiety;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public static int getPearl() {
-		return pearl;
-	}
-	public static void setPearl(int pearl) {
-		Player.pearl = pearl;
-	}
-	public static int getGarbage() {
-		return garbage;
-	}
-	public static void setGarbage(int garbage) {
-		Player.garbage = garbage;
-	}
-	public static int getSp() {
-		return sp;
-	}
-	public static void setSp(int sp) {
-		Player.sp = sp;
-	}
-	public static int getSatiety() {
-		return satiety;
-	}
-	public static void setSatiety(int satiety) {
-		Player.satiety = satiety;
+
+	public void setPearl(int pearl) {
+		this.pearl = pearl;
 	}
 
+	public void setGarbage(int garbage) {
+		this.garbage = garbage;
+	}
+
+	public void setSp(int sp) {
+		this.sp = sp;
+	}
+
+	public void setSatiety(int satiety) {
+		this.satiety = satiety;
+	}
+
+	
 }
-
 
