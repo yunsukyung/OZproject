adec01ac3ca93300d1e7ce3f0036de0ebfa1fe86
s <s@sec>
2020-04-09 14:26:05 +0900

    StartStoryView
    모양만들어놨음

--- src/com/kh/project/view/StartStoryView.java
+++ src/com/kh/project/view/StartStoryView.java
@@ -1,37 +1,82 @@
 package com.kh.project.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class StartStoryView extends JPanel{
-	
+	JPanel panel;
 	public StartStoryView(JFrame mf) {
-		
-		this.setLayout(null);
-		
-		
-		
-		
-		JLabel label = new JLabel(new ImageIcon(new ImageIcon("D:\\java\\workspace\\OZproject\\src\\image\\start\\스토리 배경.jpg").getImage().getScaledInstance(100, 100, 0)));
-		label.setBounds(0,0,100,800);
-		
-		JLabel penguin = new JLabel(new ImageIcon(new ImageIcon("D:\\java\\workspace\\OZproject\\src\\image\\start\\놀라는 펭귄.gif").getImage().getScaledInstance(50, 50, 0)));
-		penguin.setBounds(0, 0, 30, 50);
-		
-		this.add(label);
-		this.add(penguin);
-		
-		
-		
-		
+
+			
+
 	}
 	public static void main(String[] args) {
-		JFrame jf = new JFrame();
-		jf.add(new StartStoryView(jf));
-	}
-	
+		JFrame jf = new JFrame("OZ");
+		jf.setBounds(0, 0, 370, 760);
+		jf.setLayout(null);
 
-	
+		JPanel panel = new JPanel();
+		panel.setLocation(0, -40);
+		panel.setSize(370, 760);
+
+
+
+
+
+		Image icon = new ImageIcon("C:\\Users\\s\\git\\OZproject\\src\\image\\start\\background.jpg").getImage().getScaledInstance(570, 760, 0);
+		JLabel label = new JLabel(new ImageIcon(icon));
+
+
+		Image penguin = new ImageIcon("C:\\Users\\s\\git\\OZproject\\src\\image\\start\\penguin.gif").getImage().getScaledInstance(200, 200, 0);
+		JLabel label2 = new JLabel(new ImageIcon(penguin));
+		label2.setLocation(210, 310);
+		label2.setSize(200, 200);
+		
+		Image text = new ImageIcon("C:\\Users\\s\\git\\OZproject\\src\\image\\start\\text.png").getImage().getScaledInstance(350, 170, 0);
+		JLabel label3 = new JLabel(new ImageIcon(text));
+		label3.setLocation(100, 480);
+		label3.setSize(350, 300);
+		label3.setText("여기가어디지?");
+		
+		Image next = new ImageIcon("C:\\Users\\s\\git\\OZproject\\src\\image\\start\\next.png").getImage().getScaledInstance(30, 40, 0);
+		JButton button = new JButton(new ImageIcon(next));
+		button.setLocation(375, 660);
+		button.setSize(30, 40);
+		button.setBackground(Color.red);
+		button.setBorderPainted(false);
+		button.setFocusPainted(false);
+		button.setContentAreaFilled(false); 
+
+		
+		
+		
+		label.add(button, BorderLayout.CENTER);
+		label.add(label2, BorderLayout.CENTER);
+		label.add(label3, BorderLayout.CENTER);
+		panel.add(label);
+		
+
+
+		jf.add(panel);
+
+
+
+
+
+
+
+
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+
+
 }
