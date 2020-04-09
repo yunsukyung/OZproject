fde587b8684312695332900a7d8f4eab442dd3a8
user2 <user2@DESKTOP-726S1N4>
2020-04-09 21:22:04 +0900

    MainBook
    클래스안에 panel 과 label 추가
    
    image 패키지안에 book 패키지 생성 후 book 이미지 추가

--- src/com/kh/project/view/book/MainBookView.java
+++ src/com/kh/project/view/book/MainBookView.java
@@ -1,5 +1,22 @@
 package com.kh.project.view.book;
 
-public class MainBookView {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class MainBookView extends JPanel{
+	
+	
+	public MainBookView(JFrame mf) {
+		this.setLayout(null);
+		JPanel panel = new JPanel();
+		
+		JLabel back = new JLabel(new ImageIcon(new ImageIcon("C:\\Users\\user2\\git\\OZproject\\src\\image\\book\\book.PNG").getImage().getScaledInstance(370, 760, 0)));
+		back.setBounds(0, -40, 370, 760);
+		
+		panel.add(back);
+		
+		
+	}
 }
