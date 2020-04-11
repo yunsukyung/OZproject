46d4832b84ade76548a6890b5077c0cc53f11abe
s <s@sec>
2020-04-12 00:10:01 +0900

    수정수정

--- src/com/kh/project/view/shop/GameOverView1.java
+++ src/com/kh/project/view/shop/GameOverView1.java
@@ -15,26 +15,24 @@ public class GameOverView1 extends JPanel{
 		this.mf = mf;
 		
 		//위치고정 
-		this.setLayout(null);
+		
 		
 		this.setLocation(0, 0);
 		this.setSize(360, 640);
 		
 		//배경
-		Image background = new ImageIcon("src\\image\\start\\바다속gameover.png").getImage().getScaledInstance(360, 640, 0);
 		
-		JLabel label = new JLabel(new ImageIcon(background));
-		label.setBounds(0, 0, 360, 640);
 		
-		JLabel label1 = new JLabel(new ImageIcon(new ImageIcon("src\\image\\start\\GameOver1.png").getImage().getScaledInstance(360, 640, 0)));
+		JLabel label = new JLabel(new ImageIcon(new ImageIcon("src/image/start/GameOver1.png").getImage().getScaledInstance(360, 640, 0)));
 		label.setBounds(0, 0, 360, 640);
 		
 				
 				
 				
-		this.add(label1);
 		this.add(label);
-		
+	
+		mf.add(this);
+		mf.revalidate();
 		
 		
 	}
