e1dbf5ff0b89b047e512221196d84e7b8022a912
tmddu <tmddu@DESKTOP-RSHJIHM>
2020-04-13 15:34:53 +0900

    StartView
    StartView 글자 크기 변경

--- src/com/kh/project/view/StartView.java
+++ src/com/kh/project/view/StartView.java
@@ -41,19 +41,19 @@ public class StartView extends JPanel {
 		JLabel label = new JLabel(new ImageIcon(background));
 
 
-		JLabel OZ = new JLabel("      O  Z  의");
+		JLabel OZ = new JLabel("    O  Z  의");
 		OZ.setBounds(28, 57, 240, 100);
 		JTextField tf1 = new JTextField(100);
 		tf1.setBounds(0, 0, 300, 100);
-		OZ.setFont(new Font("궁서체", Font.BOLD, 30));
+		OZ.setFont(new Font("궁서체", Font.BOLD, 35));
 		OZ.setBackground(new Color(255,0,0));
 		OZ.setForeground(Color.WHITE);
 		
-		JLabel OZ1 = new JLabel("       펭 귄");
+		JLabel OZ1 = new JLabel("     펭 귄");
 		OZ1.setBounds(28, 57, 240, 300);
 		JTextField tf2 = new JTextField(100);
 		tf1.setBounds(0, 0, 300, 300);
-		OZ1.setFont(new Font("궁서체", Font.BOLD, 30));
+		OZ1.setFont(new Font("궁서체", Font.BOLD, 38));
 		OZ1.setBackground(new Color(255,0,0));
 		OZ1.setForeground(Color.WHITE);
 	
