2e7a0ea945522423c926aa64de3dde6b3727a674
dydxk <dydxk@DESKTOP-1P1VNTC>
2020-04-15 18:09:14 +0900

    asdads

--- src/com/kh/project/model/vo/Player.java
+++ src/com/kh/project/model/vo/Player.java
@@ -14,11 +14,7 @@ public class Player implements java.io.Serializable{
 	private int sp = 10;		//공격력
 	public static int satiety = 236;	//포만감
 	private boolean firstEnter = false;
-	
-	public static boolean eps1 = false;
-	public static boolean eps2 = false;
-	public static boolean eps3 = false;
-	public static boolean eps4 = false;
+
 	public Player() {}
 
 	public Player(String userId, String userPwd, String userName, 
